85797a5c0f8c185520a197ba17ea56ab02468f3e
Nestor-Rs <82476269+Nestor-Rs@users.noreply.github.com>
2022-04-04 14:30:48 -0500

    Implementacion completa de Crear Cartilla
    Ya se pueden crear nuevas cartillas, aun que no son posible visualizarla

--- app/src/main/java/com/jaguarteam/vacunaspass/registroCartilla/CrearCartilla.java
+++ app/src/main/java/com/jaguarteam/vacunaspass/registroCartilla/CrearCartilla.java
@@ -1,18 +1,35 @@
 package com.jaguarteam.vacunaspass.registroCartilla;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.jaguarteam.vacunaspass.R;
 import com.jaguarteam.vacunaspass.localData.User;
 import com.jaguarteam.vacunaspass.registroUsuarios.ConfirmacionRegistroExitoso;
+import com.jaguarteam.vacunaspass.registroUsuarios.RegistroCuenta;
+import com.orhanobut.logger.Logger;
 
 public class CrearCartilla extends AppCompatActivity {
+
+    //Creacion de usuario con FireAuht
+    private FirebaseAuth mAuth;
+    //firestore
+    private FirebaseFirestore db;
+
 
     //Manejo de fragmentos
     Fragment fragmentNow;
@@ -24,6 +41,9 @@ public class CrearCartilla extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_cartilla);
 
+        mAuth = FirebaseAuth.getInstance();
+        db= FirebaseFirestore.getInstance();
+
         fragmentManager = getSupportFragmentManager();
         fragmentTransaction = fragmentManager.beginTransaction();
 
@@ -31,6 +51,7 @@ public class CrearCartilla extends AppCompatActivity {
         fragmentTransaction.add(R.id.registroCartillaNueva, fragmentNow);
         fragmentTransaction.commit();
     }
+
     public void onClick(View view){
         switch (view.getId()) {
             case R.id.exitRNC:
@@ -43,12 +64,61 @@ public class CrearCartilla extends AppCompatActivity {
                 onBackPressed();
                 break;
             case R.id.RegistrarCartilla:
-                cambioFracment(new ConfirmacionRegistroExitoso(new User("Nestor","","","","","",0.1,0.2,"","")));
+                comprobarCampos();
+                if(comprobarCampos()!=false){
+                    createCartilla();
+                }
                 break;
             case R.id.exitCRE:
                 finish();
                 break;
         }
+    }
+
+    private void createCartilla() {
+        //ColocarText views
+        TextView nom,apeP,apeM,fNaci,pes,edadG;
+        Spinner gen,tipoS;
+        nom=findViewById(R.id.nombreCartilla);
+        apeP=findViewById(R.id.apellidoPCartilla);
+        apeM=findViewById(R.id.apellidoMCartilla);
+        gen= findViewById(R.id.generoCartilla);
+        fNaci=findViewById(R.id.fechaNacimietoCartilla);
+        pes=findViewById(R.id.pesoNacimientoCartilla);
+        edadG=findViewById(R.id.edadGestalCartilla);
+        tipoS=findViewById(R.id.tipoSangineoCartilla);
+        //Crear objeto
+        User myUser = new User(
+                mAuth.getUid(),
+                nom.getText().toString(),
+                apeP.getText().toString(),
+                apeM.getText().toString(),
+                gen.getSelectedItem().toString(),
+                fNaci.getText().toString(),
+                Double.parseDouble(pes.getText().toString()),
+                Double.parseDouble(edadG.getText().toString()),
+                tipoS.getSelectedItem().toString(),
+                "None"
+        );
+        //Crear documento
+        db.collection("InformacionUsuario").document(mAuth.getUid())
+                .collection("MiFamilia").document()
+                .set(myUser)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Logger.d("Se creo el documento", "DocumentSnapshot successfully written!");
+                        cambioFracment(new ConfirmacionRegistroExitoso(myUser));
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("No se creo el documento", "Error writing document", e);
+                        Toast.makeText(CrearCartilla.this, "Error al crear tus datos",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
     public void cambioFracment(Fragment fragment){
@@ -57,5 +127,43 @@ public class CrearCartilla extends AppCompatActivity {
         transaction.replace(R.id.registroCartillaNueva, fragment);
         transaction.addToBackStack(null);
         transaction.commit();
+    }
+
+    boolean comprobarCampos(){
+        TextView nom,apeP,apeM,fNaci,pes,edadG;
+        nom=findViewById(R.id.nombreCartilla);
+        apeP=findViewById(R.id.apellidoPCartilla);
+        apeM=findViewById(R.id.apellidoMCartilla);
+        fNaci=findViewById(R.id.fechaNacimietoCartilla);
+        pes=findViewById(R.id.pesoNacimientoCartilla);
+        edadG=findViewById(R.id.edadGestalCartilla);
+
+        if(nom.getText().toString().isEmpty()){
+            nom.setError("Ingresa tu correo");
+            return false;
+        }
+        else if(apeP.getText().toString().isEmpty()){
+            apeP.setError("Ingresa tu correo");
+            return false;
+        }
+        else if(apeM.getText().toString().isEmpty()){
+            apeM.setError("Ingresa tu correo");
+            return false;
+        }
+        else if(fNaci.getText().toString().isEmpty()){
+            fNaci.setError("Ingresa tu correo");
+            return false;
+        }
+        else if(pes.getText().toString().isEmpty()){
+            pes.setError("Ingresa tu correo");
+            return false;
+        }
+        else if(edadG.getText().toString().isEmpty()){
+            edadG.setError("Ingresa tu correo");
+            return false;
+        }
+        else{
+            return true;
+        }
     }
 }
--- app/src/main/java/com/jaguarteam/vacunaspass/registroCartilla/FormularioNuevaCartilla.java
+++ app/src/main/java/com/jaguarteam/vacunaspass/registroCartilla/FormularioNuevaCartilla.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.jaguarteam.vacunaspass.R;
 
@@ -21,5 +23,28 @@ public class FormularioNuevaCartilla extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_formulario_nueva_cartilla, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        crearSpinner();
+    }
+
+    public void crearSpinner(){
+        Spinner genero,sangre;
+        genero = (Spinner) getActivity().findViewById(R.id.generoCartilla);
+        sangre = (Spinner) getActivity().findViewById(R.id.tipoSangineoCartilla);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> sexo = ArrayAdapter.createFromResource(getActivity(), R.array.Genero,
+                android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> sanguineo = ArrayAdapter.createFromResource(getActivity(), R.array.Sangre,
+                android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        sanguineo.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sexo.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        sangre.setAdapter(sanguineo);
+        genero.setAdapter(sexo);
     }
 }
